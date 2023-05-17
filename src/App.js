bea22a1743d9711f2e74ee19ec37b88ef5cf66d7
Rohit <ranarohigrd@gmail.com>
2023-05-18 00:43:56 +0530

    Counter

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Resturant from "./component/Basics/Resturant";
+import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+import UseReducer from './component/Hooks/useReducer'
+// import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <UseReducer />;
+
+  return <Resturant />;
+
+  return < UseState />;
+
+  // return  < UseEffect/>;
+};
+
+export default App;
